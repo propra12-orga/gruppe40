cc4bcf15b34cc0bd1830caad51324cc50968e5de
Philipp Kochanski <Korr@Korr-PC.(none)>
2012-05-19 16:42:56 +0200

    Added SetX, SetY (could come in handy)

--- src/draw/Drawable.java
+++ src/draw/Drawable.java
@@ -21,6 +21,14 @@ public class Drawable {
         this.x = x;
         this.y = y;
     }
+	
+	public void setX(int x) {
+    	this.x = x;  	
+    }
+    
+    public void setY(int y) {
+    	this.y = y;	
+    }
 
     public Image getImage() {
         return img;
