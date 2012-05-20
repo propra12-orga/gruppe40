8da8e980bca3307ffdebb68118d5ea74d4c614d7
Sebastian Brink <sebastian.brink@uni-duesseldorf.de>
2012-05-20 10:58:38 +0200

    fixed window position

--- src/menu/Tutorial.java
+++ src/menu/Tutorial.java
@@ -2,10 +2,11 @@ import javax.swing.*;
 import java.io.*;
 import java.awt.Font;
 import java.awt.Dimension;
+import java.awt.Toolkit;
 
 public class Tutorial {
 	
-	public Tutorial() {
+	public Tutorial(String filepath) {
 		JFrame tutWindow = new JFrame("Tutorial");		
 		JTextArea tutText = new JTextArea();
 		
@@ -17,7 +18,7 @@ public class Tutorial {
 		
 		try {
 			//Filereader will noch nicht (klappt bei mir nur mit komplettem Pfad...)
-			FileReader fileRead = new FileReader("\\tutorialtext.txt");
+			FileReader fileRead = new FileReader(filepath);
 			BufferedReader bufferRead = new BufferedReader(fileRead);
 			while((line = bufferRead.readLine()) != null) {
 				document = document + line + "\n";
@@ -34,18 +35,22 @@ public class Tutorial {
 		//	bufferRead.close();
 		}
 		
-		//class popup is protected, so we use a factory to obtain instances
-		PopupFactory factory = PopupFactory.getSharedInstance();
-		final Popup tutorial = factory.getPopup(tutWindow, tutText, 10, 30);
-		tutorial.show();
-		
 		//gets dimensions from JTextArea (calculation based on font-size etc.)
 		Dimension windowSize = tutText.getPreferredSize();
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 		
 		//setting popup size to match JTextArea
 		tutWindow.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
 		tutWindow.setSize(windowSize.width+20, windowSize.height+40);
 		tutWindow.setVisible(true);
+		
+		Dimension center = new Dimension((int)screenSize.getWidth()/2 - tutWindow.getWidth()/2, (int)screenSize.getHeight()/2 - tutWindow.getHeight()/2);
+		tutWindow.setLocation((int)center.getWidth(), (int)center.getHeight());
+		
+		//class popup is protected, so we use a factory to obtain instances
+		PopupFactory factory = PopupFactory.getSharedInstance();
+		final Popup tutorial = factory.getPopup(tutWindow, tutText, (int)center.getWidth()+10, (int)center.getHeight()+30);
+		tutorial.show();
 	}
 
 }
