4104670571a36daaac799a6dd089c0406bdee8a1
Philipp Kochanski <Korr@Korr-PC.(none)>
2012-06-07 19:45:06 +0200

    Added xml_parser (SAX) with some example code for easy implementation Comments will be added later. map1.xml shows a simple 2x2 map

---
+++ src/xml_parser/Example_parsercode.java
@@ -0,0 +1,54 @@
+package xml_parser;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.xml.parsers.ParserConfigurationException;
+import org.xml.sax.SAXException;
+
+/**
+ * XML should look like this: <?xml version="1.0"?> 
+ * <Map>
+ * <Width>2</Width>
+ * <Height>2</Height>
+ * <Line_1> 
+ * <Element>NormalField</Element>
+ * <Element>NormalField</Element> 
+ * </Line_1> 
+ * <Line_2>
+ * <Element>IndestructibleField</Element>
+ * <Element>IndestructibleField</Element> 
+ * </Line_2> 
+ * </Map> 
+ * 
+ * Map generating code should be changed to: 
+ * - New Constructor with arraylist<string>,width,height 
+ * - If possible implement code idea by keiner
+ */
+
+
+public class Example_parsercode {
+	/**
+	 * @param args
+	 * @throws ParserConfigurationException
+	 * @throws SAXException
+	 * @throws IOException
+	 */
+	public static void main(String[] args) throws ParserConfigurationException,
+			SAXException, IOException {
+
+		ArrayList<String> MapArray = null;
+
+		MapParser testing = new MapParser("map1.xml");
+		testing.parse();
+		MapArray = testing.getMapElements();
+		System.out.println("Width: " + testing.get_width() + "   " + "Height: "
+				+ testing.get_height());
+
+		System.out.println("MapElements: " + MapArray.get(0) + " "
+				+ MapArray.get(1) + " " + MapArray.get(2) + " "
+				+ MapArray.get(3) + " ");
+
+	}
+
+}
---
+++ src/xml_parser/MapHandler.java
@@ -0,0 +1,100 @@
+package xml_parser;
+
+import java.util.ArrayList;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.helpers.DefaultHandler;
+
+/**
+ * @author Korr
+ * 
+ */
+public class MapHandler extends DefaultHandler {
+
+	private String Element_test;
+	private String Element;
+	private ArrayList<String> MapElements;
+	private int width;
+	private int height;
+
+	/**
+	 * 
+	 */
+	public MapHandler() {
+		this.width = 0;
+		this.height = 0;
+		this.Element_test = null;
+		MapElements = new ArrayList<String>();
+	}
+
+	public void startDocument() {
+		// System.out.println("Dokumentverarbeitung gestartet." + "\n");
+
+	}
+
+	public void endDocument() {
+		// System.out.println("Dokumentverarbeitung beendet.");
+		// System.out.println("Gespeicherte Inhalte:");
+		// System.out.println("Width: " + this.width);
+		// System.out.println("Height: " + this.height);
+		// System.out.println("MapElements: " + MapElements.get(0) + " " +
+		// MapElements.get(1) + " " + MapElements.get(2) + " " +
+		// MapElements.get(3) + " ");
+	}
+
+	public void startElement(String namespaceURI, String localName,
+			String qName, Attributes atts) {
+
+		if (qName.equals("Width"))
+			this.Element_test = "Width";
+		if (qName.equals("Height"))
+			this.Element_test = "Height";
+		if (qName.equals("Element"))
+			this.Element_test = "Element";
+
+		// System.out.print(qName + " ");
+
+	}
+
+	@Override
+	public void endElement(String uri, String localName, String qName) {
+
+		if (Element_test.equals("Width")) {
+			this.width = Integer.parseInt(Element);
+		}
+
+		if (Element_test.equals("Height")) {
+			this.height = Integer.parseInt(Element);
+		}
+		if (Element_test.equals("Element")) {
+			addValue(Element);
+		}
+		this.Element_test = "";
+
+	}
+
+	public void characters(char ch[], int start, int length) {
+		this.Element = new String(ch, start, length);
+		// System.out.print(new String(ch, start, length));
+	}
+
+	private void addValue(String value) {
+		if (value != null) {
+			this.MapElements.add(value);
+			// System.out.println(Element);
+
+		}
+	}
+
+	public ArrayList<String> getMapElements() {
+		return this.MapElements;
+	}
+
+	public int get_width() {
+		return this.width;
+	}
+
+	public int get_height() {
+		return this.height;
+	}
+}
