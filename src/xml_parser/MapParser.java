ab15e979427527069cfe326e4cb371cca4c8ffe5
Philipp Kochanski <Korr@Korr-PC.(none)>
2012-06-13 16:31:46 +0200

    Added try-catch block - No need to add throws in Map generating code. Best error handling thus far.

--- src/xml_parser/MapParser.java
+++ src/xml_parser/MapParser.java
@@ -1,58 +1,72 @@
 package xml_parser;
-
-import java.io.IOException;
-import java.util.ArrayList;
-
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.parsers.SAXParser;
-import javax.xml.parsers.SAXParserFactory;
-
-import org.xml.sax.SAXException;
-import org.xml.sax.XMLReader;
-
-public class MapParser {
-
-	private ArrayList<String> MapArray;
-	private String map;
-	private int width;
-	private int height;
-
-	/**
-	 * @param map
-	 */
-	public MapParser(String map) {
-		this.map = map;
-	}
-
-	/**
-	 * @throws IOException
-	 * @throws SAXException
-	 * @throws ParserConfigurationException
-	 */
-	public void parse() throws IOException, SAXException,
-			ParserConfigurationException {
-		SAXParserFactory testing = SAXParserFactory.newInstance();
-		SAXParser saxParser = testing.newSAXParser();
-		XMLReader reader = saxParser.getXMLReader();
-		MapHandler handler = new MapHandler();
-		reader.setContentHandler(handler);
-		reader.parse(this.map);
-		this.MapArray = handler.getMapElements();
-		this.width = handler.get_width();
-		this.height = handler.get_height();
-
-	}
-
-	public ArrayList<String> getMapElements() {
-		return this.MapArray;
-	}
-
-	public int get_width() {
-		return this.width;
-	}
-
-	public int get_height() {
-		return this.height;
-	}
-
-}
+     
+    import java.io.IOException;
+    import java.util.ArrayList;
+     
+    import javax.xml.parsers.ParserConfigurationException;
+    import javax.xml.parsers.SAXParser;
+    import javax.xml.parsers.SAXParserFactory;
+     
+    import org.xml.sax.SAXException;
+    import org.xml.sax.XMLReader;
+     
+    public class MapParser {
+     
+            private ArrayList<String> MapArray;
+            private String map;
+            private int width;
+            private int height;
+     
+            /**
+             * @param map
+             */
+            public MapParser(String map) {
+                    this.map = map;
+            }
+     
+            /**
+             * @throws IOException
+             * @throws SAXException
+             * @throws ParserConfigurationException
+             */
+            public void parse(){
+                try{
+                    SAXParserFactory testing = SAXParserFactory.newInstance();
+                    SAXParser saxParser = testing.newSAXParser();
+                    XMLReader reader = saxParser.getXMLReader();
+                    MapHandler handler = new MapHandler();
+                    reader.setContentHandler(handler);
+                    reader.parse(this.map);
+                    this.MapArray = handler.getMapElements();
+                    this.width = handler.get_width();
+                    this.height = handler.get_height();
+                }
+                catch (ParserConfigurationException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+    catch (SAXException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+           
+    catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+     
+            }
+     
+            public ArrayList<String> getMapElements() {
+                    return this.MapArray;
+            }
+     
+            public int get_width() {
+                    return this.width;
+            }
+     
+            public int get_height() {
+                    return this.height;
+            }
+     
+    }
