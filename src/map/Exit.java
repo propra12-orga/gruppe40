1436addb8f15616fea7a850d7fbb301fe7c2a30e
Sebastian Brink <sebastian.brink@uni-duesseldorf.de>
2012-06-25 16:52:24 +0200

    not working

--- src/map/Exit.java
+++ src/map/Exit.java
@@ -4,10 +4,29 @@ public class Exit extends Field {
 
     public Exit() {
         super(0);
+        this.duration = 180;
+    }
+    
+    @Override
+    public int getFrame() {
+    	double u = (System.currentTimeMillis() - t)%(double)duration;
+        int nx = getFrameCountX();
+        int ny = getFrameCountY();
+        return (int)(nx*ny*u);
+    }
+    
+    @Override
+    public int getFrameCountX() {
+        return 9;
+    }
+
+    @Override
+    public int getFrameCountY() {
+        return 2;
     }
 
     @Override
     public String getPath() {
-        return "Exit.gif";
+        return "Exit.png";
     }
 }
