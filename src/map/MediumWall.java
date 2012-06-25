526cac3a20d6b728e9f2bd1a6aa1459d59f84b34
Sebastian Brink <sebastian.brink@uni-duesseldorf.de>
2012-06-25 14:46:02 +0200

    resized

--- src/map/MediumWall.java
+++ src/map/MediumWall.java
@@ -5,5 +5,10 @@ public class MediumWall extends DestructibleWall {
     public MediumWall() {
         super(2);
     }
+    
+    @Override
+    public String getPath() {
+        return "MediumWall.jpg";
+    }
 
 }
